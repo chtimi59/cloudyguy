0a2266149d772d939fc2df836b80c8a073981179
Jan D''Orgeville <jdorgeville@gmail.com>
2018-03-15 08:42:16 -0400

    add comments

--- raspberrypi/cloudyguy_webpage.sh
+++ raspberrypi/cloudyguy_webpage.sh
@@ -4,8 +4,10 @@ set -e
 
 PATH=/sbin:/bin:/usr/sbin:/usr/bin
 
+#copy webpage code base
 #sudo cp -R cloudyguy_webpage /etc
+
+#run script when IF is mounted
 #sudo cp cloudyguy_webpage.sh /etc/network/if-up.d/cloudyguy_webpage && sudo chmod a+x /etc/network/if-up.d/cloudyguy_webpage
 
-#sudo /usr/sbin/irbrgd start
 sudo node /etc/cloudyguy_webpage/server.js &
